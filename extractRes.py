bdb7f4de2ab5308896467b921b39bfd2c4da7b7c
coldmooon <howtoliy@gmail.com>
2023-07-18 22:26:28 +0800

    Deconstruct the main function into multiple subfunctions.

--- extractRes.py
+++ extractRes.py
@@ -6,8 +6,7 @@ import os
 import numpy as np
 
 
-def main():
-    file_path = "dltData.json"
+def lotteryResult(file_path='dltData.json'):
 
     with open(file_path, "r") as f:
         # Load the JSON data from the file
@@ -15,10 +14,7 @@ def main():
    
     total = X['value']['total']
 
-    # Access the loaded JSON data
-    # print(X['value']['list'][0]['lotteryDrawResult'])
-    # print(X['value']['list'][2446]['lotteryDrawResult'])
-
+    # Y is a lottery results string list.
     Y = [ X['value']['list'][i]['lotteryDrawResult'] for i in range(total)]
     T = [ {'lotteryDrawTime':X['value']['list'][i]['lotteryDrawTime'],
            'lotteryDrawNum': X['value']['list'][i]['lotteryDrawNum']} for i in range(total)]
@@ -44,21 +40,51 @@ def main():
 
     # Set diagonal elements to 0
     np.fill_diagonal(similarity_matrix, 0)
-
-    # Round the similarity matrix to the nearest integer
-    # rounded_matrix = np.round(similarity_matrix).astype(int)
     
-    # Find the positions where the similarity matrix equals 5
-    positions = np.argwhere(similarity_matrix == 5)
+    return similarity_matrix, one_hot_matrix, T
+
+def lotteryStats(collision, similarity_matrix, T):
+    # Find the positions where the similarity matrix equals the given number.
+    positions = np.argwhere(similarity_matrix == collision)
     
     # Get the total count of number 5 occurrences
-    count = len(positions)
+    count = len(positions)/2
     
     # Output the positions and count
-    print("Positions:")
+    print("重复出现 " + str(collision) + " 个数的位置:")
     for pos in positions:
-        print(pos, ": ", T[pos[0]]['lotteryDrawNum'],' and ', T[pos[1]]['lotteryDrawNum'])
-    print("Total count:", count)
+        print(pos, ": ", T[pos[0]]['lotteryDrawNum'], ' 和 ',  T[pos[1]]['lotteryDrawNum'])
+    print("总计:", count)
+    
+def takeLook(your_number, one_hot_matrix, T):
+    
+    one_hot_vec = np.zeros((1, 35))
+    
+    # Iterate through each element in Y
+    for element in your_number:
+        column_index = int(element) - 1
+        one_hot_vec[0, column_index] = 1
+   
+    # Calculate the similarity matrix
+    res = np.dot(one_hot_matrix, one_hot_vec.T).astype(int)
+    res = res.flatten()
+    
+    collision_dict = {}
 
-if __name__ == '__main__':
-    main()
+    values_to_find = [5, 4, 3]
+
+    for value in values_to_find:
+        collision_dict[value] = np.argwhere(res == value).flatten().tolist()
+
+    for value, positions in collision_dict.items():
+        count = len(positions)
+        if count > 0:
+            print("\n重了 " + str(value) + " 个数的有 " + str(count) + " 次，分别在：")
+    #         print(f"Value {value}: {positions} \n")
+            for pos in positions:
+                print("  :", T[pos]['lotteryDrawNum'], " 期")
+        else:
+            print("\n没有重了 " + str(value) + " 个数的情况。")
+ 
+    return res
+    
